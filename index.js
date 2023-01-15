1338d4834615d61bfce73188ec13f4c17d251cb4
Thiha Aung <thihaaungkyaw@qslogics.com>
2023-01-15 23:58:02 +0630

    Section 5 expressjs starts

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 const http = require("http");
-const routes = require("./routes");
 
-const server = http.createServer(routes);
+const express = require("express");
+
+const app = express();
+const server = http.createServer(app);
 
 server.listen(9090);
 
